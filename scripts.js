e281fcc0fd9a5021973768bb628a2d39bf138ac9
prankad19 <prankad19@mail.wlu.edu>
2017-03-16 09:42:20 -0400

    exercise work

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var temp = prompt("Please enter a number");
+if (Number(temp)) {
+  var table = "<table border=1>" + "<th>" + "Fahrenheit" + "</th>" + "<th>" + "Celcius" + "</th>";
+  for (var i=0; i<10; i++) {
+    var temp2 = (5/9 * (temp - 32));
+    table = table + "<tr>" + "<td>" + temp + "</td>" + "<td>" + temp2 + "</td>" + "</tr>";
+    document.getElementById("unique").innerHTML = table;
+    var temp = (temp - 5);
+    table = table + "</table";
+  }
+}else {
+  alert = ("Error");
+}
